0bdca9faaf6ab073e3e8dd97abaef6e730ce7f39
shuai_global <shuai_global@atguigu.com>
2017-10-13 10:49:15 +0800

    new branche content

--- src/main/java/com/atguigu/git/huashan/BiXieJianPu.java
+++ src/main/java/com/atguigu/git/huashan/BiXieJianPu.java
@@ -5,6 +5,12 @@ public class BiXieJianPu {
 	public static void main(String[] args) {
 		
 		System.out.println("Master content...");
+		System.out.println("new branch content ...");
+		System.out.println("new branch content ...");
+		System.out.println("new branch content ...");
+		System.out.println("new branch content ...");
+		System.out.println("new branch content ...");
+		System.out.println("new branch content ...");
 		
 	}
 
